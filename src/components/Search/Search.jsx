c478ce7e4fbfc2632298668fe5888c59a65bab32
Paballo Modutwane <109858262+Plainpablo@users.noreply.github.com>
2026-03-25 00:15:34 +0200

    search pop up updates

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -21,19 +21,71 @@ const Search = () => {
     };
   }, [isSearchOpen]);
 
+  // Navigation list
+  const navigationList = [
+    {
+      icon: MagnifyingGlassIcon,
+      title: "Go to Home",
+      shortcutLeft: "G",
+      shortcutRight: "K",
+    },
+  ];
+
   return (
     <div
       ref={searchRef}
-      className={`fixed z-20 flex-col w-full max-w-[550px] p-8 right-0 left-0 mx-auto top-[calc(13vh-32px)]  bg-white shadow-[0_15px_50px_rgba(0,0,0,0.35)] rounded-[10px] h-fit ${isSearchOpen ? "flex" : "hidden"}`}
+      className={`fixed z-20 flex-col w-full max-w-[650px] right-0 left-0 mx-auto top-[calc(13vh-32px)]  bg-white shadow-[0_15px_50px_rgba(0,0,0,0.35)] rounded-[10px] h-fit ${isSearchOpen ? "flex" : "hidden"}`}
     >
-      <div className="flex">
-        <div className="flex">
-            <MagnifyingGlassIcon className="size-6" />
-            <input className="w-full" type="text" placeholder="Search or type a command..." />
-          <div>
-            <kbd className="px-[5px] mx-[1px] bg-[#00000012] rounded-[3px]">Ctrl</kbd>
-            <kbd className="px-[5px] mx-[1px] bg-[#00000012] rounded-[3px]">K</kbd>
+      <div className="flex px-3 flex-col">
+        <div className="flex w-full h-[50px] justify-center items-center border-b-[1px] border-[#f5f5f5]">
+          <div className="ml-2 size-6 w-6 h-6 flex justify-center items-center">
+            <MagnifyingGlassIcon className="size-4 w-4 h-4" />
           </div>
+          <input
+            className="w-full mx-[14px] outline-none"
+            type="text"
+            placeholder="Search or type a command..."
+          />
+          <div>
+            <kbd className="px-[5px] mx-[1px] bg-[#00000012] rounded-[3px] text-[#444] font-normal">
+              Ctrl
+            </kbd>
+            <kbd className="px-[5px] mx-[1px] bg-[#00000012] rounded-[3px] text-[#444] font-normal ">
+              K
+            </kbd>
+          </div>
+        </div>
+
+        <div className="flex flex-col">
+          <p className="text-[#666] text-xs pl-3 pt-3 pr-1 pb-1 text-start">
+            Navigation
+          </p>
+
+          {navigationList.map((nav, index) => (
+            <div
+              key={index}
+              className="h-[42px] flex items-center justify-between"
+            >
+              <div className="flex items-center">
+                <div className="ml-2 size-6 w-6 h-6 flex justify-center items-center">
+                  <nav.icon className="size-4 w-4 h-4 " />
+                </div>
+                <p className="text-[13px]">{nav.title}</p>
+              </div>
+
+              <div>
+                <kbd className="px-[5px] mx-[1px] bg-[#00000012] rounded-[3px] text-[#444] font-normal ">
+                  {nav.shortcutLeft}
+                </kbd>
+                <kbd className="px-[5px] mx-[1px] text-[#444]  font-normal ">
+                  then
+                </kbd>
+                <kbd className="px-[5px] mx-[1px] bg-[#00000012] rounded-[3px] text-[#444] font-normal ">
+                  {nav.shortcutRight}
+                </kbd>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
