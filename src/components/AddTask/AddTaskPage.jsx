6e679182776b199a7b074a6f003f1b164f8faf74
Paballo Modutwane <109858262+Plainpablo@users.noreply.github.com>
2026-03-30 17:14:24 +0200

    add task page

---
+++ src/components/AddTask/AddTaskPage.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useRef, useState } from "react";
+import { useModal } from "../../context/PopUpModalContext";
+import DatePickerAction from "./Controls/DatePickerAction";
+import AttachmentAction from "./Controls/AttachmentAction";
+import Priority from "./Controls/Priority";
+
+const AddTaskPage = () => {
+  const [focused, setFocused] = useState(false);
+  const { isAddTaskOpen, setIsAddTaskOpen } = useModal();
+  const modalRef = useRef(null);
+
+  // Handle add task and search pop menus
+  function handlePopupMenu() {
+    setIsAddTaskOpen(false);
+  }
+
+  // Close pop with outside click
+  useEffect(() => {
+    function handleClickOutSide(e) {
+      if (modalRef.current && !modalRef.current.contains(e.target)) {
+        setIsAddTaskOpen(false);
+      }
+    }
+
+    if (isAddTaskOpen) {
+      document.addEventListener("mousedown", handleClickOutSide);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutSide);
+    };
+  }, [isAddTaskOpen]);
+
+  return (
+    <div
+      ref={modalRef}
+      className={`fixed z-20 flex-col w-full max-w-[550px] p-8 right-0 left-0 mx-auto top-[calc(13vh-32px)] flex`}
+    >
+      <div
+        className={`w-full bg-white border-[1px] border-[#e6e6e6] rounded-[10px] h-fit right-0 left-0 mx-auto`}
+      >
+        <div className="p-3 flex flex-col border-b-[1px] border-[#e6e6e6]">
+          <div className="relative flex">
+            <input
+              className="outline-none "
+              type="text"
+              name=""
+              id=""
+              placeholder="Title of the task"
+              onFocus={() => setFocused(true)}
+              onBlur={() => setFocused(false)}
+            />
+            {!focused && (
+              <span className="absolute left-[-2px] animate-blink pointer-events-none top-[-8px] text-[22px]">
+                |
+              </span>
+            )}
+          </div>
+          <input
+            className="outline-none mt-1"
+            type="text"
+            name=""
+            id=""
+            placeholder="Description"
+          />
+
+          <div className="flex gap-2 mt-2">
+            <DatePickerAction />
+            <AttachmentAction />
+            <Priority />
+          </div>
+        </div>
+        <div className="m-4 flex gap-2 justify-end">
+          <button
+            className="bg-[#0000000a] px-3 py-1 rounded-[5px]"
+            onClick={() => handlePopupMenu()}
+          >
+            <span>Cancel</span>
+          </button>
+          <button className="px-3 py-1 rounded-[5px] bg-[#eda59e] text-white cursor-not-allowed">
+            <span>Add Task</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddTaskPage;
--- src/components/pages/Today.jsx
+++ src/components/pages/Today.jsx
@@ -1,48 +1,62 @@
 import { PlusIcon } from "@heroicons/react/24/outline";
 import bgImage from "../../assets/images/vacation-vector.png";
 import { useState } from "react";
+import AddTaskPage from "../AddTask/AddTaskPage";
 
 const Today = () => {
   const [isHover, setIsHover] = useState(false);
+  const [addTaskModal, setAddTaskModal] = useState(false);
 
   function handleAddTaskHover(value) {
     value === "MouseEnter"
       ? setIsHover(true)
       : value === "MouseLeave"
-        ? setIsHover(false) 
+        ? setIsHover(false)
         : "";
+  }
+
+  function handleAddTask() {
+    setAddTaskModal(true);
   }
   return (
     <div className="flex flex-col">
-      <div className="flex mx-14 mb-3">
-        <h1 className="text-2xl font-bold">Today</h1>
-      </div>
-      <div className="px-14">
-        <button
-          className={`flex items-center ${isHover ? "text-[#d33322]" : ""}`}
-          onMouseEnter={() => handleAddTaskHover("MouseEnter")}
-          onMouseLeave={() => handleAddTaskHover("MouseLeave")}
-        >
-          <PlusIcon className={`size-[13px] mr-3 ${isHover? "text-white bg-[#d33322]  rounded-[50%]" : "text-[#d33322]"}`} />
-          Add task
-        </button>
-      </div>
+      {!addTaskModal && (
+        <>
+          <div className="flex mx-14 mb-3">
+            <h1 className="text-2xl font-bold">Today</h1>
+          </div>
+          <div className="px-14">
+            <button
+              className={`flex items-center ${isHover ? "text-[#d33322]" : ""}`}
+              onMouseEnter={() => handleAddTaskHover("MouseEnter")}
+              onMouseLeave={() => handleAddTaskHover("MouseLeave")}
+              onClick={handleAddTask}
+            >
+              <PlusIcon
+                className={`size-[13px] mr-3 ${isHover ? "text-white bg-[#d33322]  rounded-[50%]" : "text-[#d33322]"}`}
+              />
+              Add task
+            </button>
+          </div>
 
-      <div className="flex flex-col justify-center items-center max-w-[360px] mx-auto">
-        <img
-          src={bgImage}
-          alt="vacation vector image"
-          className="max-w-[294px]"
-        />
-        <div>
-          <p className="text-[#202020] font-semibold my-[6px]">
-            Have a marvelous day off, Plain!
-          </p>
-          <p className="text-[#666] text-sm px-6 mx-6">
-            Schedule days off in the Productivity tab of your Settings menu.
-          </p>
-        </div>
-      </div>
+          <div className="flex flex-col justify-center items-center max-w-[360px] mx-auto">
+            <img
+              src={bgImage}
+              alt="vacation vector image"
+              className="max-w-[294px]"
+            />
+            <div>
+              <p className="text-[#202020] font-semibold my-[6px]">
+                Have a marvelous day off, Plain!
+              </p>
+              <p className="text-[#666] text-sm px-6 mx-6">
+                Schedule days off in the Productivity tab of your Settings menu.
+              </p>
+            </div>
+          </div>
+        </>
+      )}
+      {addTaskModal && <AddTaskPage />}
     </div>
   );
 };
