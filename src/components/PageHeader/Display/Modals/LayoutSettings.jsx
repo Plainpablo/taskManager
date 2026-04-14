9f5452fea8b66c9a25bdf527bc83f962dd4880bb
Plain Pablo <plainpablo@gmail.com>
2026-04-14 15:12:21 +0200

    Build CustomDropList

--- src/components/PageHeader/Display/Modals/LayoutSettings.jsx
+++ src/components/PageHeader/Display/Modals/LayoutSettings.jsx
@@ -2,43 +2,70 @@ import {
   ChevronDownIcon,
   QuestionMarkCircleIcon,
 } from "@heroicons/react/24/outline";
+import CustomDropList from "../../../UI/CustomDropList";
 
 export const LayoutSettings = () => {
+  const sortOptions = [
+    "smart",
+    "Manual",
+    "Name",
+    "Assignee",
+    "Date",
+    "Date added",
+    "Deadline",
+    "Priority",
+    "Project",
+    "Workspace",
+  ];
   return (
-    <div className="absolute top-8 right-0 bg-white w-max z-40">
+    <div className="absolute top-8 right-0 bg-white w-max z-30 rounded-[10px] py-[6px]">
       <div className="w-[300px] flex flex-col gap-[6px]">
-        <div className="flex justify-between items-center py-[2px] px-1">
-          <span>Layout</span>
+        <div className="flex justify-between items-center py-[2px] px-[12px]">
+          <span className="text-[#202020] font-semibold text-sm">Layout</span>
           <QuestionMarkCircleIcon className="size-4" />
         </div>
-        <div className="border-b-[#666] border-b-[1px]"></div>
+
+        <div className="border-b-[#eee] border-b-[1px]"></div>
+
         <div>
           {/* Sort */}
-          <div>
+          <div className="px-[6px]">
             <div className="flex justify-between items-center py-[2px] px-[6px]">
-              <span>Sort</span>
+              <span className="text-[#202020] font-semibold text-sm">Sort</span>
               <div className="w-7 h-7 flex justify-center items-center bg-[#0000000a] rounded-[5px]">
                 <ChevronDownIcon className="size-4" />
               </div>
             </div>
+
             <div className="flex justify-between items-center py-[2px] px-[6px]">
-              <span>Grouping</span>
-              <div className="w-[160px] h-[28px] pr-[6px] ml-[10px] border-[#e6e6e6] border-[1px] rounded-[5px] flex">
-                <select className="w-full" name="" id=""></select>
-              </div>
+              <span className="text-[#202020] font-normal text-sm">
+                Grouping
+              </span>
+              <CustomDropList
+                sortOptions={sortOptions}
+                width="w-[160px]"
+                height="h-[28px]"
+              />
             </div>
-            <div className="flex justify-between items-center py-[2px] px-[6px]">
-              <span>Sorting</span>
-              <div className="w-[160px] h-[28px] pr-[6px] pl-[10px] border-[#e6e6e6] border-[1px] rounded-[5px] flex">
-                <select className="w-full" name="" id=""></select>
-              </div>
+
+            <div className="flex justify-between items-center py-[2px] px-[6px] relative">
+              <span className="text-[#202020] font-normal text-sm">
+                Sorting
+              </span>
+              <CustomDropList
+                sortOptions={sortOptions}
+                width="w-[160px]"
+                height="h-[28px]"
+              />
             </div>
           </div>
 
           {/* Filter */}
-          <div>
+          <div className="px-[6px]">
             <div className="flex justify-between items-center py-[2px] px-[6px]">
-              <span>Filter</span>
+              <span className="text-[#202020] font-semibold text-sm">
+                Filter
+              </span>
               <div className="w-7 h-7 flex justify-center items-center bg-[#0000000a] rounded-[3px]">
                 <ChevronDownIcon className="size-4" />
               </div>
---
+++ src/components/ui/CustomDropList.jsx
@@ -0,0 +1,84 @@
+import { CheckIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
+import { Check } from "phosphor-react";
+import { useEffect, useRef, useState } from "react";
+
+const CustomDropList = ({ sortOptions, width, height }) => {
+  const [currentOption, setCurrentOption] = useState(sortOptions[0]);
+  const [isDropDownOpen, setIsDropDownOpen] = useState(true);
+  const dropDownRef = useRef(null);
+
+  // Display clicked option and close drop down
+  function handleSelectOption(option) {
+    setCurrentOption(option);
+    setIsDropDownOpen(false);
+  }
+
+  // close drop down - out side click
+  useEffect(() => {
+    function handleClickOutSide(event) {
+      if (dropDownRef.current && !dropDownRef.current.contains(event.target)) {
+        setIsDropDownOpen(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutSide);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutSide);
+    };
+  }, [isDropDownOpen]);
+
+  return (
+    <div
+      className={`${width} ${height} border-[#e6e6e6] border-[1px] rounded-[5px] flex relative`}
+    >
+      {/* Current option */}
+      <div
+        className="w-full flex items-center justify-between absolute top-0 bottom-0 pl-[10px] pr-[6px]"
+        onClick={() => setIsDropDownOpen((prev) => !prev)}
+      >
+        <span className="text-sm text-[#202020] font-normal text-left">
+          {currentOption[0].toUpperCase() + currentOption.slice(1)}
+        </span>
+        <ChevronDownIcon className="size-4 text-[#666]" />
+      </div>
+      {/* Drop down */}
+      {isDropDownOpen && (
+        <div
+          ref={dropDownRef}
+          className="flex flex-col bg-white absolute top-7 max-h-[280px] overflow-auto p-[6px] border-[#eee] border-[1px] shadow-[0px_4px_10px_0px_#0000001a,0px_8px_18px_4px_#0000000d)] w-full rounded-[10px] overflow-x-hidden z-40"
+        >
+          {sortOptions.map((option, index) =>
+            currentOption === option ? (
+              <div
+                key={index}
+                className="flex gap-2 items-center px-[6px] py-1 hover:bg-[#eee] hover:rounded-[5px]"
+                onClick={() => setIsDropDownOpen(false)}
+              >
+                <Check size={16} color="#202020" weight="bold" />
+                <span className="text-sm text-[#202020] font-normal">
+                  {option[0].toUpperCase() + option.slice(1)}
+                </span>
+              </div>
+            ) : (
+              <div
+                key={index}
+                className="flex gap-2 items-center px-[6px] py-1 hover:bg-[#eee] hover:rounded-[5px]"
+                onClick={() => handleSelectOption(option)}
+              >
+                <div className="size-4"></div>
+                <span
+                  key={index}
+                  className="text-sm text-[#202020] font-normal"
+                >
+                  {option[0].toUpperCase() + option.slice(1)}
+                </span>
+              </div>
+            ),
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CustomDropList;
